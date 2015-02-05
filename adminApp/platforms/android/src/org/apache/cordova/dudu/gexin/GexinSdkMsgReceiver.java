2308b45bb13a629052ba70af712ee17e8aad3463
joe <hzhou9@gmail.com>
2015-02-06 01:37:55 +0800

    add release support

--- adminApp/platforms/android/src/org/apache/cordova/dudu/gexin/GexinSdkMsgReceiver.java
+++ adminApp/platforms/android/src/org/apache/cordova/dudu/gexin/GexinSdkMsgReceiver.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.igexin.sdk.Consts;
+import com.igexin.sdk.PushConsts;
 
 public class GexinSdkMsgReceiver extends BroadcastReceiver {
     
@@ -16,9 +16,9 @@ public class GexinSdkMsgReceiver extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
 		Bundle bundle = intent.getExtras();
 		Log.i("GexinSdkDemo", "onReceive() action=" + bundle.getInt("action"));
-		switch (bundle.getInt(Consts.CMD_ACTION)) {
+		switch (bundle.getInt(PushConsts.CMD_ACTION)) {
 
-		case Consts.GET_MSG_DATA:
+		case PushConsts.GET_MSG_DATA:
 			// 获取透传数据
 			byte[] payload = bundle.getByteArray("payload");
 
@@ -28,11 +28,10 @@ public class GexinSdkMsgReceiver extends BroadcastReceiver {
                 Device.device.onMsgData(data);
 			}
 			break;
-		case Consts.GET_CLIENTID:
+		case PushConsts.GET_CLIENTID:
                 // 获取ClientID(CID)
                 // 第三方应用需要将CID上传到第三方服务器，并且将当前用户帐号和CID进行关联，以便日后通过用户帐号查找CID进行消息推送
                 pushid = bundle.getString("clientid");
-                Log.i("GexinSdkDemo", "Got Clientid:" + pushid);
                 Device.device.onPushID(pushid);
 			break;
 		default:
