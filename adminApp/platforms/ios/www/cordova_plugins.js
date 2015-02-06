22d5d133c09c51bccf75a7a1f8dfe7d776d75091
joe <hzhou9@gmail.com>
2015-02-06 18:16:23 +0800

    inappbrowser

--- adminApp/platforms/ios/www/cordova_plugins.js
+++ adminApp/platforms/ios/www/cordova_plugins.js
@@ -6,12 +6,20 @@ module.exports = [
         "clobbers": [
             "device"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
+        "id": "org.apache.cordova.inappbrowser.inappbrowser",
+        "clobbers": [
+            "window.open"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.dudu.gexin": "0.2.14-dev"
+    "org.apache.cordova.dudu.gexin": "0.2.14-dev",
+    "org.apache.cordova.inappbrowser": "0.5.4"
 }
 // BOTTOM OF METADATA
 });
