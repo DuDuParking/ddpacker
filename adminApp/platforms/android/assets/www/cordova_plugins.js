42727371ed0dab7190840a18d1bbb8bb58358a6c
joe <hzhou9@gmail.com>
2015-02-10 21:50:40 +0800

    add network information support

--- adminApp/platforms/android/assets/www/cordova_plugins.js
+++ adminApp/platforms/android/assets/www/cordova_plugins.js
@@ -13,13 +13,29 @@ module.exports = [
         "clobbers": [
             "window.open"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/network.js",
+        "id": "org.apache.cordova.network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/Connection.js",
+        "id": "org.apache.cordova.network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "org.apache.cordova.dudu.gexin": "0.2.14-dev",
-    "org.apache.cordova.inappbrowser": "0.5.4"
+    "org.apache.cordova.inappbrowser": "0.5.4",
+    "org.apache.cordova.network-information": "0.2.14"
 }
 // BOTTOM OF METADATA
 });
