3ed6295e88c4305b5c7951f73fb834ed14e842ae
Ryun <ryunhe@gmail.com>
2015-06-12 22:30:35 +0800

    fix navigator launcher bug

--- driverApp/plugins/uk.co.workingedge.phonegap.plugin.LaunchNavigator/www/android/launchnavigator.js
+++ driverApp/plugins/uk.co.workingedge.phonegap.plugin.LaunchNavigator/www/android/launchnavigator.js
@@ -1,7 +1,7 @@
-/*
+cordova.define("uk.co.workingedge.phonegap.plugin.LaunchNavigator.LaunchNavigator", function(require, exports, module) { /*
  * Copyright (c) 2014 Dave Alden  (http://github.com/dpa99c)
  * Copyright (c) 2014 Working Edge Ltd. (http://www.workingedge.co.uk)
- *  
+ *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without
@@ -10,10 +10,10 @@
  * copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following
  * conditions:
- *  
+ *
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
- *  
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
  * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -22,9 +22,9 @@
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
- *  
+ *
  */
-    
+
 var launchnavigator = {};
 
 /**
@@ -43,64 +43,16 @@ var launchnavigator = {};
  * @param {Function} errorCallback (optional) - A callback which will be called when plugin encounters an error.
  * This callback function have a string param with the error.
  */
-launchnavigator.navigate = function(destination, start, successCallback, errorCallback) {
-    var dType, sType = "none";
-    if(typeof(destination) == "object"){
-        dType = "pos";
-    }else{
-        dType = "name";
-    }
-
-    if(start){
-        if(typeof(start) == "object"){
-            sType = "pos";
-        }else{
-            sType = "name";
-        }
-    }
-
+launchnavigator.navigate = function(type, destination, start, successCallback, errorCallback) {
     return cordova.exec(
         successCallback,
         errorCallback,
         'LaunchNavigator',
         'navigate',
-        [dType, destination, sType, start]);
+        [type, destination, start]);
 
 };
 
-    
-/**
- * Opens navigator app to navigate to given lat/lon destination
- *
- * @param {Number} lat - destination latitude as decimal number
- * @param {Number} lon - destination longitude as decimal number
- * @param {Function} successCallback - The callback which will be called when plugin call is successful.
- * @param {Function} errorCallback - The callback which will be called when plugin encounters an error.
- * This callback function have a string param with the error.     
- */
-launchnavigator.navigateByLatLon = function(lat, lon, successCallback, errorCallback) {
-    if(typeof(console) != "undefined") console.warn("launchnavigator.navigateByLatLon() has been deprecated and will be removed in a future version of this plugin. Please use launchnavigator.navigate()");
-    return cordova.exec(successCallback,
-        errorCallback,
-        'LaunchNavigator',
-        'navigateByLatLon',
-        [lat, lon]);
-};
-
-/**
- * Opens navigator app to navigate to given place name destination
- *
- * @param {String} name - place name to navigate to
- * @param {Function} successCallback - The callback which will be called when plugin call is successful.
- * @param {Function} errorCallback - The callback which will be called when plugin encounters an error.
- * This callback function have a string param with the error.     
- */
-launchnavigator.navigateByPlaceName = function(name, successCallback, errorCallback) {
-    if(typeof(console) != "undefined") console.warn("launchnavigator.navigateByPlaceName() has been deprecated and will be removed in a future version of this plugin. Please use launchnavigator.navigate()");
-    return cordova.exec(successCallback,
-        errorCallback,
-        'LaunchNavigator',
-        'navigateByPlaceName',
-        [name]);
-};
 module.exports = launchnavigator;
+
+});
