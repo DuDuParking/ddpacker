9812bf98a5e1c2dfc54d2df5369d87da3af9d867
Ryun <ryunhe@gmail.com>
2015-06-12 16:59:26 +0800

    remove build files

---
+++ driverApp/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/ryun/Dropbox/Workspaces/ddpacker/driverApp/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/ryun/Dropbox/Workspaces/ddpacker/driverApp/platforms/android/CordovaLib/AndroidManifest.xml \
